70cccbe72272a5e88cc25771be4dff4202482d14
Ratidrobzhevi <ratidrobzhevi@gmail.com>
2024-10-28 18:55:25 -0400

    second commit

---
+++ level 03/homework/homework.py
@@ -0,0 +1,56 @@
+# 1)
+
+print("Nika" * 100)
+
+# 2)
+
+# კონკადინაცია არის სტრინგის სტრინგზე შეეთება
+
+print("5" + "5") # 55
+
+
+# 3)
+
+# გამოასწორეთ შემდეგი კოდი:
+# city => "London':
+# print"city)
+
+city = "London"
+print(city)
+
+# 4)
+
+# რიცხვების საშუალო არითმეტიკული გამოითვლება ფორმულით: რიცხვთა ჯამი გაყოფილი მათ რაოდენობაზე
+
+age1 = 12
+age2 = 50
+age3 = 55
+
+sum = age1 + age2 + age3
+
+print(sum / 3)
+
+# 5)
+
+num1 = 2
+num2 = 3
+
+squeared_num1 = num1 * num1
+squeared_num2 = num2 * num2
+
+print(squeared_num1 + squeared_num2)
+
+# 6)
+
+number1 = 10
+number2 = 5
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+# 7)
+
+# კომენტარი ხელსაწყო, რომელიც გვეხმარება მარტივად ავღწეროთ კოდი.
+# კომპიუტერი კოდის კითხვისას აიგნორებს(არ კითხულობს) კომენტარს
---
+++ level 04/homework/homework.py
@@ -0,0 +1,35 @@
+# 1)
+
+points = 50
+points = 80
+points = 100
+
+print(points)
+
+# 2)
+
+# snake_case არის ცვლადის შექმნის ტექნიკა, როდესაც ორი ან მეტი სიტყვისგან შემდგარი ცვლადის შექმნისას, სიტყვებს შორის იწერება "_"
+
+my_age = 12
+
+my_favorite_car = "Lamborgini"
+
+my_favorite_color = "green"
+
+# 3)
+
+number = 4
+age = 12
+number1 = 10
+
+print(number * age * number1)
+
+# 4)
+
+num1 = 20
+num2 = 15
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
