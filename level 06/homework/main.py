e4a3675570cdef7661fa0584f1a7e10f255c6e69
Ratidrobzhevi <ratidrobzhevi@gmail.com>
2024-11-13 17:23:04 -0500

    third commit

---
+++ level 06/homework/main.py
@@ -0,0 +1,22 @@
+# 1)
+
+favorite_sport = input("Please enter a fevorite sport: ")
+print(favorite_sport)
+
+# 2)
+
+name = input("Please enter your name: ")
+surname = input("Please enter your surname: ")
+age = input("Please enter your age: ")
+
+print("Your name is " + name + ". Your surname is " + surname + ". You are " + age + " years old")
+
+# 3)
+
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
