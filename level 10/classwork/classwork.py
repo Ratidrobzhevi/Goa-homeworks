086c2ca937cf8ed9bed1b311150faf55a5dc71ac
Ratidrobzhevi <ratidrobzhevi@gmail.com>
2024-12-12 15:44:33 -0500

    second commit

---
+++ level 10/classwork/classwork.py
@@ -0,0 +1,22 @@
+
+print(80 > 70)
+
+print(70 > 60)
+
+print(60 > 50)
+
+print(50 > 40)
+
+print(40 > 30)
+
+
+
+print(10 > 20)
+
+print(20 > 30)
+
+print(30 > 40)
+
+print(40 > 50)
+
+print(50 > 60)
