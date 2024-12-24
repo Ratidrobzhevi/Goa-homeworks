75ea1a12e5c809dc36fe0a15b0f9275ede977e56
Ratidrobzhevi <ratidrobzhevi@gmail.com>
2024-12-24 14:31:50 -0500

    second commit

---
+++ level 10/homework/homework.py
@@ -0,0 +1,21 @@
+# Explisity არის ის რომ ჩვენი ხელითვე შევცვლით მაგალითად ციფრი 4 მაგალითად #
+#number= (4)
+
+#number= str(4)#
+
+# Explisity = ტიპის შეცვლა გამიზნულად
+
+
+
+
+# implisity = მონცემთა ტიპის შეცვლა ავტომატურად ჩვენი ჩარევის გარეშე
+
+
+
+
+
+num1=input ("give me number:  ")
+
+num2=input ("give me another number:  ")
+
+print(num1 > num2)
