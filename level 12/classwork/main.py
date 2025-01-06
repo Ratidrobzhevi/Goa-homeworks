6c806adb54dc73bfde32fcad808dc000f9f8a79d
Ratidrobzhevi <ratidrobzhevi@gmail.com>
2025-01-06 17:16:10 -0500

    fourth commit

---
+++ level 12/classwork/main.py
@@ -0,0 +1,14 @@
+
+bool1 = 10 > 20
+bool2 = 2 < 5
+bool3 = 10 >= 10
+bool4 = 25 <= 7
+bool5 = 2 == 20
+bool6 = 10 != 11
+
+print(bool1)
+print(bool2)
+print(bool3)
+print(bool4)
+print(bool5)
+print(bool6)
