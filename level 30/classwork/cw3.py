c930f091111ef5b6020475e8ab7f6fd5bb522038
Ratidrobzhevi <ratidrobzhevi@gmail.com>
2025-08-16 00:52:53 -0400

    second commit

---
+++ level 30/classwork/cw3.py
@@ -0,0 +1,11 @@
+# საწყისი სია
+fruits = ["Melon", "Orange", "Banana", "Watermelon", "Kiwi"]
+
+# ბოლო ელემენტის ამოგდება
+fruits.pop()
+
+# მესამე ინდექსზე "Pomegranate"-ის ჩასმა
+fruits.insert(3, "Pomegranate")
+
+# შედეგის დაბეჭდვა
+print(fruits)
